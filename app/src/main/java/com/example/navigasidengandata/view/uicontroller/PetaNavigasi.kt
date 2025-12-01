c23e61e89a435053e1c420610579f99d86bd33f4
RamaSandhy <rshandy07@gmail.com>
2025-12-02 01:02:57 +0700

    Menambahkan composable SiswaApp sebagai entry point navigasi aplikasi

--- app/src/main/java/com/example/navigasidengandata/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/navigasidengandata/view/uicontroller/PetaNavigasi.kt
@@ -1,2 +1,18 @@
 package com.example.navigasidengandata.view.uicontroller
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+
+@Composable
+fun SiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    HostNavigasi(
+        navController = navController,
+        modifier = modifier
+    )
+}
