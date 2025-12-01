3d5069c8a2cf334c3e80d2d0d08a26d099765092
RamaSandhy <rshandy07@gmail.com>
2025-12-02 01:16:44 +0700

    Menyelesaikan komponen SiswaTopAppBar dengan CenterAlignedTopAppBar dan tombol back

--- app/src/main/java/com/example/navigasidengandata/view/AppBar.kt
+++ app/src/main/java/com/example/navigasidengandata/view/AppBar.kt
@@ -1,9 +1,16 @@
 package com.example.navigasidengandata.view
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -15,3 +22,19 @@ fun SiswaTopAppBar(
     navigateUp: () -> Unit = {}
 ) {
 
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back)
+                    )
+                }
+            }
+        }
+    )
+}
