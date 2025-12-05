4fa081d561a31a9c15e0ff7bb0eeeb6ccb6d4743
RamaSandhy <rshandy07@gmail.com>
2025-12-05 14:46:27 +0700

    perbaikan

--- app/src/main/java/com/example/navigasidengandata/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/navigasidengandata/viewmodel/provider/PenyediaViewModel.kt
@@ -1,30 +1,37 @@
-package com.example.navigasidengandata.viewmodel.provider
+package com.example.pertemuan9.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pertemuan9.viewmodel.HomeViewModel
+import com.example.pertemuan9.repositori.AplikasiSiswa
+import com.example.pertemuan9.viewmodel.DetailViewModel
+import com.example.pertemuan9.viewmodel.EditViewModel
+import com.example.pertemuan9.viewmodel.EntryViewModel
+import com.example.roomdatabase.repositori.AplikasiSiswa
+
 
 object PenyediaViewModel {
-    val Factory = viewModelFactory {
-
+    val factory = viewModelFactory {
         initializer {
-            val repositorySiswa =
-                (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
-                    .container
-                    .repositorySiswa
-
-            EntryViewModel(repositorySiswa = repositorySiswa)
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
 
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
 
         initializer {
-            val repositorySiswa =
-                (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
-                    .container
-                    .repositorySiswa
+            DetailViewModel(this.createSavedStateHandle(), aplikasiSiswa().container.repositoriSiswa)
+        }
 
-            EditViewModel(repositorySiswa = repositorySiswa)
+        initializer {
+            EditViewModel(this.createSavedStateHandle(), aplikasiSiswa().container.repositoriSiswa)
         }
     }
 }
 
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
