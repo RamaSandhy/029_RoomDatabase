9ffa8b0717bf8aec7608fb9c698468d5a94a104d
RamaSandhy <rshandy07@gmail.com>
2025-12-05 14:45:32 +0700

    perbaikan

--- app/src/main/java/com/example/navigasidengandata/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/navigasidengandata/viewmodel/provider/PenyediaViewModel.kt
@@ -1,2 +1,30 @@
 package com.example.navigasidengandata.viewmodel.provider
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+
+        initializer {
+            val repositorySiswa =
+                (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
+                    .container
+                    .repositorySiswa
+
+            EntryViewModel(repositorySiswa = repositorySiswa)
+        }
+
+
+        initializer {
+            val repositorySiswa =
+                (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
+                    .container
+                    .repositorySiswa
+
+            EditViewModel(repositorySiswa = repositorySiswa)
+        }
+    }
+}
+
