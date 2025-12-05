c6133dd8aa510aa85434c2fff021464a4a7a53d5
RamaSandhy <rshandy07@gmail.com>
2025-12-05 15:17:33 +0700

    perbaikan

--- app/src/main/java/com/example/navigasidengandata/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/navigasidengandata/repositori/RepositoriSiswa.kt
@@ -1,23 +1,35 @@
-package com.example.navigasidengandata.repositori
+package com.example.pertemuan9.repositori
+
 
 import com.example.navigasidengandata.room.Siswa
 import com.example.navigasidengandata.room.SiswaDao
 import kotlinx.coroutines.flow.Flow
 
-
-interface RepositorySiswa {
-
+interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
-
+    fun getSiswaStream(id: Int): Flow<Siswa?>
+    suspend fun deleteSiswa(siswa: Siswa)
     suspend fun insertSiswa(siswa: Siswa)
+
+    suspend fun updateSiswa(siswa: Siswa)
 }
 
-// Usage
-class OfflineRepositorySiswa(
+class OfflineRepositoriSiswa(
     private val siswaDao: SiswaDao
-) : RepositorySiswa {
+) : RepositoriSiswa {
 
-    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
+    override fun getAllSiswaStream(): Flow<List<Siswa>> =
+        siswaDao.getAllSiswa()
 
-    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+    override fun getSiswaStream(id: Int): Flow<Siswa?> =
+        siswaDao.getSiswa(id)
+
+    override suspend fun insertSiswa(siswa: Siswa) =
+        siswaDao.insert(siswa)
+
+    override suspend fun deleteSiswa(siswa: Siswa) =
+        siswaDao.delete(siswa)
+
+    override suspend fun updateSiswa(siswa: Siswa) =
+        siswaDao.update(siswa)
 }
